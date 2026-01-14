ed1713f719b488cc702a29c2c6a5be241dda4f53
Györfi Mihály <1tc-gyomih@moriczref.lan>
2026-01-14 08:51:24 +0100

    service content done

--- kisujForgeFrontend/src/app/services/challenge.ts
+++ kisujForgeFrontend/src/app/services/challenge.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Challenge } from '../models/challenge';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Challenge {
-  
+@Injectable({ providedIn: 'root' })
+export class ChallengeService {
+  private apiUrl = 'http://localhost:8000/api/challenges';
+
+  constructor(private http: HttpClient) {}
+
+  getAll() { return this.http.get<Challenge[]>(this.apiUrl); }
+  getById(id: number) { return this.http.get<Challenge>(`${this.apiUrl}/${id}`); }
+  create(data: Challenge) { return this.http.post(this.apiUrl, data); }
+  update(id: number, data: Challenge) { return this.http.patch(`${this.apiUrl}/${id}`, data); }
+  delete(id: number) { return this.http.delete(`${this.apiUrl}/${id}`); }
 }
