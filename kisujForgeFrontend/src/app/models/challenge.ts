cf7c0846c55cdeaef5f53ab106acb5357bc47ac8
Györfi Mihály <1tc-gyomih@moriczref.lan>
2026-01-14 08:43:45 +0100

    models content done

--- kisujForgeFrontend/src/app/models/challenge.ts
+++ kisujForgeFrontend/src/app/models/challenge.ts
@@ -1,2 +1,11 @@
 export interface Challenge {
+  id?: number;
+  title: string;
+  category: 'CODE' | 'DESIGN' | 'DATA' | 'SOFT';
+  difficulty: 'EASY' | 'MEDIUM' | 'HARD';
+  rewardPoints: number;
+  startDate: string;
+  endDate: string;
+  isActive: boolean;
+  description: string;
 }
