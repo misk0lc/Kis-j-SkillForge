224bb320bb6c4e75e7cea4d87acc68719cc78828
Györfi Mihály <1tc-gyomih@moriczref.lan>
2026-01-14 08:52:32 +0100

    routing done

--- kisujForgeFrontend/src/app/app.routes.ts
+++ kisujForgeFrontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { ChallengeList } from './components/challenge-list/challenge-list';
+import { ChallengeForm } from './components/challenge-form/challenge-form';
+import { ChallengeDetails } from './components/challenge-details/challenge-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Dashboard },
+  { path: 'challenges', component: ChallengeList },
+  { path: 'challenges/add', component: ChallengeForm },
+  { path: 'challenges/:id', component: ChallengeDetails },
+  { path: 'challenges/:id/edit', component: ChallengeForm },
+];
